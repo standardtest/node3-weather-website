c4b9b453791bd14e947918db680d42eb07760984
swt-shalaka <demostandard123@gmail.com>
2020-03-05 19:07:24 +0530

    Setup app for hureku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ""
     //console.log(location)
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
     //console.log(response)
     response.json().then((data) =>{
         //console.log(data)
