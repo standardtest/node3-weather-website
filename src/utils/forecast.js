d44cd95c176671d1a13e5d99f214cab676623fbf
swt-shalaka <demostandard123@gmail.com>
2020-03-06 11:39:36 +0530

    Add high and Low forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
     }else if(body.error){
         callback('Unable to find Location', undefined)
     }else{
-        callback(undefined, body.daily.data[0].summary + 'It is currently '+ body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' % chance of Rain')
+        console.log(body.daily.data[0])
+        callback(undefined, body.daily.data[0].summary + 'It is currently '+ body.currently.temperature + ' degrees out. The high today is '+ body.daily.data[0].temperatureHigh + ' with a low of '+ body.daily.data[0].temperatureLow + '.There is a ' + body.currently.precipProbability + ' % chance of Rain')
     }
  })
 }
